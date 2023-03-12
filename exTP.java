986209b09db047a2c8bfaf55935b1fc6d37f5565
Beatriz Fulgêncio <beatrizfulgencio03@gmail.com>
2023-03-12 10:45:20 -0300

    crud

--- exTP.java
+++ exTP.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class exTP {
 
-    public static int qntde = 0/*3856*/;
+    public static int qntde = 0/* 3856 */;
 
     public static void read(Crud bfr) throws FileNotFoundException {
         File file = new File("netflix.csv");
@@ -27,58 +27,45 @@ public class exTP {
         sc.close();
 
     }
-    public static void aleaRead(Crud bfr) throws FileNotFoundException {
-        File file = new File("netflix.csv");
-        Scanner sc = null;
-
-        int i = 0;
-        try {
-            sc = new Scanner(file);
-            while (/* sc.hasNextLine() */ i < 3856) {
-                Movie movie = new Movie((long)Math.floor(Math.random()*(3856)));
-                movie.read(sc.nextLine());
-                bfr.writeMovie(movie);
-                i++;
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        sc.close();
-
-    }
-
 
     public static void main(String[] args) throws Exception {
         Crud byteFileRandler = new Crud("movies.db");
+        Scanner sc = new Scanner(System.in);
+        // carga da base de dados selecionada
+        read(byteFileRandler);
 
-        // read(byteFileRandler);
+        // // Ler um registro (id)
+        // System.out.println("Digite o id do registro a ser procurado (com 4 dígitos, EX: 0020): ");
+        // String id = sc.nextLine();
+        // byteFileRandler.read(id);
 
-        // aleaRead(byteFileRandler);
+        // // Atualizar um registro
+        // System.out.println("Digite o id do registro a ser atualizado (com 4 dígitos, EX: 0020): ");
+        // String id = sc.nextLine();
+        // byteFileRandler.update(id);
 
+        // // deletar um registro
+        // System.out.println("Digite o id do registro a ser deletado (com 4 dígitos, EX: 0020): ");
+        // String id = sc.nextLine();
+        // byteFileRandler.delete(id);
+
+        // criar novo registro
+        // byteFileRandler.create();
+
+        // limpar arquivo db
         // byteFileRandler.clear();
-        // byteFileRandler.read("0000");
 
-        // byteFileRandler.update("0002");
-
-        // byteFileRandler.update("0000");
-
-        Sort fileSort = new Sort("movies.db");
+        //-----------------------------------------------------------------
+        // Sort fileSort = new Sort("movies.db");
 
         // fileSort.clear();
 
         // fileSort.intercalacaoBalanceadaComum();
+
+        // fileSort.read();
+
         // fileSort.intercalacaoSegmentosVariaveis();
-// 
-        // fileSort.read("a");
-        // fileSort.read(0);
-        fileSort.read();
-        // byteFileRandler.create();
-        // byteFileRandler.delete("0099");
-        // byteFileRandler.update("0002");
-        // System.out.println(byteFileRandler.select("0002"));
-        // System.out.println(byteFileRandler.select("0003"));
+
     }
 
 }
